5f9b38a64139b7554d161cc9c103cbbc0c8397bf
Sean Bradley <seanwasere@gmail.com>
2021-06-07 20:27:11 +0100

    three r129, added import maps to html

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
-import * as THREE from '/build/three.module.js';
-import {OrbitControls} from '/jsm/controls/OrbitControls.js';
-import Stats from '/jsm/libs/stats.module.js';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
 
