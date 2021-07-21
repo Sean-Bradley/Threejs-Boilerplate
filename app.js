c3dea50a39723054fb16f26df3c84c9b98210118
Sean Bradley <seanwasere@gmail.com>
2021-07-21 21:54:17 +0100

    three r130

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express()
 const path = require('path')
 
 app.use(express.static(__dirname + '/public'))
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
-app.listen(3000, () =>
-  console.log('Visit http://127.0.0.1:3000')
-);
+app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
--- public/client.js
+++ public/client.js
@@ -1,47 +1,51 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
-    wireframe: true
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    wireframe: true,
+})
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
-}, false);
+window.addEventListener(
+    'resize',
+    () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    },
+    false
+)
 
-const stats = Stats();
-document.body.appendChild(stats.dom);
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
 var animate = function () {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    controls.update();
-    render();
-    stats.update();
-};
-
-function render() {
-    renderer.render(scene, camera);
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    controls.update()
+    render()
+    stats.update()
 }
 
-animate();
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
