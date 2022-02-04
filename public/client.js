3b4187910e23fdc72099e1047dc871d6177f0532
Sean Bradley <seanwasere@gmail.com>
2022-02-04 22:40:52 +0000

    updates

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min'
 
 const scene = new THREE.Scene()
 
